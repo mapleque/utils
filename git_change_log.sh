e744424aaeb10a28911b9eafcad247aa8ba5bb5b
杨阳 <yangyang@licaigc.com>
2018-11-16 14:42:27 +0800

    hotfix: git_change_log.sh support no tag condition

--- git_change_log.sh
+++ git_change_log.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-git log --oneline --no-merges `git describe --abbrev=0 --tags`..HEAD|cut -c 9-|sort
+
+
+git describe --abbrev=0 --tags > /dev/null 2>&1
+if [[ $? == 0 ]]; then
+    git log --oneline --no-merges `git describe --abbrev=0 --tags`..HEAD|cut -c 9-|sort
+else
+    git log --format="- %ad %s" --no-merges --date=short
+fi
